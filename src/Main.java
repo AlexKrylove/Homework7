a584db580cf2b4e5415e690602c2c54305a891d6
AlexKrylov <krylofffff93@gmail.com>
2022-11-07 23:19:18 +0300

    Task1

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Task1.1");
+        int [] weight = new int [3];
+        weight[0]=1;
+        weight[1]=2;
+        weight[2]=3;
+        System.out.println(weight[0]);
+        System.out.println(weight[1]);
+        System.out.println(weight[2]);
+        System.out.println("Task1.2");
+        float [] weight1 = {1.57f, 7.654f, 9.986f};
+        System.out.println("Task1.3");
+        int [] weight2 = {2, 4, 6 , 8, 10};
+
+
+
+
+    }
+}
